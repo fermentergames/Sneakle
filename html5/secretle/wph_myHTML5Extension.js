008b92a4b431c054150951539cd49a3e48f6a908
fermentergames <andrew@fermentergames.com>
2024-10-30 20:41:13 -0400

    html folder

---
+++ html5/secretle/wph_myHTML5Extension.js
@@ -0,0 +1,114 @@
+
+
+/*function showShare() {
+	let thebutt = document.getElementById("shareButton");
+	//let thebuttShow = document.getElementById("btnShow");
+	//let thebuttHide = document.getElementById("btnHide");
+	thebutt.style.visibility = "visible";
+	//thebuttShow.style.visibility = "hidden";
+	//thebuttHide.style.visibility = "visible";
+	console.log("SHOW share button");
+}
+
+function hideShare() {
+	let thebutt = document.getElementById("shareButton");
+	//let thebuttShow = document.getElementById("btnShow");
+	//let thebuttHide = document.getElementById("btnHide");
+	thebutt.style.visibility = "hidden";
+	//thebuttShow.style.visibility = "visible";
+	//thebuttHide.style.visibility = "hidden";
+	console.log("HIDE share button");
+}*/
+
+function showElemID(elemID) {
+	let thething = document.getElementById(elemID);
+	thething.style.visibility = "visible";
+	console.log("SHOW "+elemID);
+}
+
+function hideElemID(elemID) {
+	let thething = document.getElementById(elemID);
+	thething.style.visibility = "hidden";
+	console.log("HIDE "+elemID);
+}
+
+function addClassElemID(elemID,className) {
+	let thething = document.getElementById(elemID);
+	//thething.style.visibility = "visible";
+	thething.classList.add(className);
+	console.log("ADD "+className+" to "+elemID);
+}
+
+function removeClassElemID(elemID,className) {
+	let thething = document.getElementById(elemID);
+	//thething.style.visibility = "hidden";
+	thething.classList.remove(className);
+	console.log("REMOVE "+className+" from "+elemID);
+}
+
+function changeQuery(key1,value1,key2,value2) {
+
+	//THAINORAYMFUJCET_2-6-10-15
+
+	console.log("New Query Should Be "+key1+"="+value1);
+	console.log("New Query Should Be "+key2+"="+value2);
+
+	if ('URLSearchParams' in window) {
+	  const url = new URL(window.location)
+	  url.searchParams.set(key1, value1)
+	  url.searchParams.set(key2, value2)
+	  location.assign(url);
+	  //history.pushState(null, '', url);
+	}
+
+
+
+}
+
+
+ function loadParentQueryString() {
+
+ 	console.log("loadParentQueryString happening");
+
+    var queryStringKeyValue = window.parent.location.search.replace('?', '').split('&');
+    var qsJsonObject = {};
+    if (queryStringKeyValue != '') {
+        for (i = 0; i < queryStringKeyValue.length; i++) {
+            qsJsonObject[queryStringKeyValue[i].split('=')[0]] = queryStringKeyValue[i].split('=')[1];
+        }
+    }
+
+	console.log(qsJsonObject);
+
+    return qsJsonObject;
+
+}
+
+function useParentLoadBoardQueryString() {
+
+	console.log("useParentLoadBoardQueryString happening");
+	let parentsLoadBoard = loadParentQueryString().loadBoard;
+	console.log("Parent's loadBoard is "+parentsLoadBoard);
+	
+	return parentsLoadBoard;
+
+}
+
+function useParentLoadSecretQueryString() {
+
+	console.log("useParentLoadSecretQueryString happening");
+	let parentsLoadSecret = loadParentQueryString().loadSecret;
+	console.log("Parent's loadSecret is "+parentsLoadSecret);
+	
+	return parentsLoadSecret;
+
+}
+
+
+
+function copyToClipboard(string) {
+
+	let custom_code = document.querySelector(".exportCode").select();
+	document.execCommand('copy'); 
+    
+}
